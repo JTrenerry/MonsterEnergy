37f59dd96027dc2cfc71116656ce54811b472898
JTrenerry <jacksontrenerry01@gmail.com>
2024-03-24 12:36:30 +1000

    coles done

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Complies the TS to JS and moves it to the correct folder
-rm -r $HOME/.local/share/cinnamon/desklets/MonsterEnergy@cinnamon.org
-mkdir $HOME/.local/share/cinnamon/desklets/MonsterEnergy@cinnamon.org
-cp ./src/* $HOME/.local/share/cinnamon/desklets/MonsterEnergy@cinnamon.org/
--- node/index.js
+++ node/index.js
@@ -1,40 +1,39 @@
 const { Scraper, Root, CollectContent } = require('nodejs-web-scraper');
 
-//(async () => {
-//    const config = {
-//        baseSiteUrl: `https://www.woolworths.com.au/shop/search/products?searchTerm=monster%20energy`,
-//        startUrl: `https://www.woolworths.com.au/shop/search/products?searchTerm=monster%20energy`,           
-//        headers: {
-//            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
-//
-//        }, 
-//    }
-//
-//    const mydivs = new Set();
-//
-//    const getElementContent = (content, pageaddress) => {
-//        console.log(content);
-//        console.log(pageaddress);
-//        mydivs.add(content);
-//    }
-//
-//    const scraper = new Scraper(config);
-//
-//    const root = new Root();
-//    
-//
-//    const title = new CollectContent(`h1`, {name: `title`}); //any of these will fit.
-//
-//    root.addOperation(title);
-//
-//    await scraper.scrape(root);
-//
-//    console.log(mydivs);
-//
-//    return mydivs;
-//
-//})();
-//
+(async () => {
+    const config = {
+        baseSiteUrl: `https://www.woolworths.com.au/shop/search/products?searchTerm=monster%20energy`,
+        startUrl: `https://www.woolworths.com.au/shop/search/products?searchTerm=monster%20energy`,           
+        headers: {
+            'User-Agent': `Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0`,
+        }, 
+    }
+
+    const mydivs = new Set();
+
+    const getElementContent = (content, pageaddress) => {
+        console.log(content);
+        console.log(pageaddress);
+        mydivs.add(content);
+    }
+
+    const scraper = new Scraper(config);
+
+    const root = new Root();
+    
+
+    const title = new CollectContent(`h1`, {name: `title`}); //any of these will fit.
+
+    root.addOperation(title);
+
+    await scraper.scrape(root);
+
+    console.log(mydivs);
+
+    return mydivs;
+
+})();
+
 
 
 async function coles() {
@@ -47,7 +46,9 @@ async function coles() {
 
     const regex = /SPECIAL\$\d+\.\d+\$\d+\.\d+ per \d{1,2}l(?:pick any \d+ for \$\d+\.\d+)?/g;
 
-    const regex2 = /\s\$[\d.]+\sper\s1l/g;
+    const regex2 = /\$\d+\.\d+\s+per\s+1L/g;
+
+    const regex3 = /Save\s.*$/g;
 
     const getElementContent = (content, pageaddress) => {
         
@@ -58,8 +59,12 @@ async function coles() {
         data = data.replace("500mL", "");
         data = data.replace("500ml", "");
         data = data.replace(regex2, " "); // not working 
-        data = data.replace("4 Pack", " ").replace("4 pack", "");
+        data = data.replace("4 pack", "4 Pack").replace("4 Pack", "");
         data = data.replace("for ", "for");
+        data = data.replace("SPECIAL", "");
+        data = data.replace("Currently unavailable", "");
+        data = data.replace(regex3, "");
+        data = data.replace(" Pick", "Pick");
         data = data.trim();
         mydivs.add(data);
     }
@@ -83,14 +88,15 @@ async function coles() {
 async function main() {
     var fs = require('fs');
     text = await coles();
-    // removes the file
-    fs.unlink('../src/output.txt', function (err) {
-        if (err) throw err;
-    });
     // writes to file
+    fs.writeFile('./output.txt', "", function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
     for (var item of text) {
-        fs.appendFile('../src/output.txt', item + '\n', function (err) {
+        fs.appendFile('./output.txt', item + "\n", function (err) {
             if (err) throw err;
+            console.log('Saved!');
         });
     }
 }
