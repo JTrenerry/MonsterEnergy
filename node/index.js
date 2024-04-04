8902bb7b5a445c3862cb5c15ce5f930f1e70bb19
JTrenerry <jacksontrenerry01@gmail.com>
2024-04-04 17:43:22 +1000

    dsd

--- node/index.js
+++ node/index.js
@@ -1,47 +1,16 @@
 const { Scraper, Root, CollectContent } = require('nodejs-web-scraper');
 
-(async () => {
-    const config = {
-        baseSiteUrl: `https://www.woolworths.com.au/shop/search/products?searchTerm=monster%20energy`,
-        startUrl: `https://www.woolworths.com.au/shop/search/products?searchTerm=monster%20energy`,           
-        headers: {
-            'User-Agent': `Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0`,
-        }, 
-    }
-
-    const mydivs = new Set();
-
-    const getElementContent = (content, pageaddress) => {
-        console.log(content);
-        console.log(pageaddress);
-        mydivs.add(content);
-    }
-
-    const scraper = new Scraper(config);
-
-    const root = new Root();
-    
-
-    const title = new CollectContent(`h1`, {name: `title`}); //any of these will fit.
-
-    root.addOperation(title);
-
-    await scraper.scrape(root);
-
-    console.log(mydivs);
-
-    return mydivs;
-
-})();
-
-
-
 async function coles() {
    const config = {
         baseSiteUrl: `https://www.coles.com.au/search?q=monster%20energy`,
-        startUrl: `https://www.coles.com.au/search?q=monster%20energy`,           
+        startUrl: `https://www.coles.com.au/search?q=monster%20energy`,   
+        logPath: './logs',
+        showConsoleLogs:false,        
+        headers: {
+            'User-Agent': `curl/7.87.0`,
+            'accept': '*/*',
+        }, 
    }
-
     const mydivs = new Set();
 
     const regex = /SPECIAL\$\d+\.\d+\$\d+\.\d+ per \d{1,2}l(?:pick any \d+ for \$\d+\.\d+)?/g;
@@ -51,7 +20,7 @@ async function coles() {
     const regex3 = /Save\s.*$/g;
 
     const getElementContent = (content, pageaddress) => {
-        
+    
         data = content.replace(regex, '');
         data = data.split('|')[1]; 
         data = data.replace("Add", "");
@@ -65,6 +34,7 @@ async function coles() {
         data = data.replace("Currently unavailable", "");
         data = data.replace(regex3, "");
         data = data.replace(" Pick", "Pick");
+        data = data.replace("NEW ", "");
         data = data.trim();
         mydivs.add(data);
     }
@@ -73,13 +43,11 @@ async function coles() {
 
     const root = new Root();
 
-    const title = new CollectContent(`.sc-9243a325-5, .iXddGd, .coles-targeting-ProductTileHeaderWrapper`, {getElementContent}); //any of these will fit.
+    const title = new CollectContent(`.coles-targeting-ProductTileHeaderWrapper`, {getElementContent});
 
     root.addOperation(title);
 
     await scraper.scrape(root);
-
-    console.log(mydivs);
 
     return mydivs;
 
@@ -91,12 +59,19 @@ async function main() {
     // writes to file
     fs.writeFile('./output.txt', "", function (err) {
         if (err) throw err;
-        console.log('Saved!');
     });
+
+    fs.appendFile('./output.txt', "UQ:\nVending Machine - $3\nEzy Mart - $4.99\n", function (err) {
+        if (err) throw err;
+    });
+
+    fs.appendFile('./output.txt', "\nColes:\n", function (err) {
+        if (err) throw err;
+    });
+
     for (var item of text) {
         fs.appendFile('./output.txt', item + "\n", function (err) {
             if (err) throw err;
-            console.log('Saved!');
         });
     }
 }
